2b1e7d97d57f8d008c28cfeed144f1b9f27693a0
空白 <484137278@qq.com>
2026-06-24 21:25:50 +0800

    处理样式异常

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,8 @@ import { defineConfig, fontProviders } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	site: 'https://kongbaier.github.io',
+	base: '/personlize',
 	integrations: [mdx(), sitemap()],
 	fonts: [
 		{
